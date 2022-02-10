b498fec9cd43549c39374091cf4ab662c3e05a18
Lebogang Lottering <lebolottering2@outlook.com>
2022-02-10 08:56:01 +0200

    completed the repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let answer = '';
+    if (num < 0){
+        return 'ERROR';
+    }
+    for(let i = 0; i < num; i++){
+        answer += string;
+    }
+    return answer;
 
 };
 
