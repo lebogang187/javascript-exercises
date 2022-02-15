9a4bb0b0a251d73e528114e93ae9fc0c4fdfe7d0
Lebogang Lottering <lebolottering2@outlook.com>
2022-02-15 11:22:48 +0200

    completed the reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+let normalString = "Hello";
 
+const reverseString = function(normalString) {
+    let reversedString = "";
+    let holder = normalString.split("");
+    for(let i = holder.length - 1; i >= 0; i--){
+        reversedString += holder[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
