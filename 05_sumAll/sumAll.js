ab6d9168b97399fe558ca94687d8bb5b0ae14a96
Lebogang Lottering <lebolottering2@outlook.com>
2022-02-18 15:55:48 +0200

    completed the sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,39 @@
 const sumAll = function() {
+    
+    if (typeof arguments[0] != 'number'){  // checks if the argument is a number
+        return 'ERROR';
+    }
+
+    if (typeof arguments[1] != 'number'){  // checks if the argument is a number
+        return 'ERROR';
+    }
+
+    if (arguments[0] < 0){                 // checks if the number is negative
+        return 'ERROR';
+    }
+
+    if (arguments[1] < 0){                 // checks if the number is negative
+        return 'ERROR';
+    }
+
+    let smallest;
+    let biggest;
+
+   if (arguments[0] > arguments[1]){
+       biggest = arguments[0];
+       smallest = arguments[1];
+    } else {
+       biggest = arguments[1];
+       smallest = arguments[0];       //checks the biggest number
+    }
+
+    let sum = 0;
+
+    for(let i = smallest; i <= biggest; i++){
+        sum += i;
+      
+    }
+    return sum;
 
 };
 
