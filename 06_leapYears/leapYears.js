81d6dca0631596defff4a756176c822ee7f6f922
Lebogang Lottering <lebolottering2@outlook.com>
2022-02-25 15:49:57 +0200

    completed the leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
 const leapYears = function() {
+    if (arguments[0] % 4 === 0 && arguments[0] % 100 === 0 && arguments[0] % 400 === 0) {
+        return true;
+    } else if (arguments[0] % 4 === 0 && arguments[0] % 100 === 0) {
+        return false;
+    }  else if (arguments[0] % 4 === 0) {
+        return true;
+    } else if (arguments[0] % 400 === 0) {
+        return true; 
+    } else if (arguments[0] % 100 === 0) {
+        return false;
+    } else {
+        return false;
+    }
 
 };
 
