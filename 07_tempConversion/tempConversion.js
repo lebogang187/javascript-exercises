9e729258ac9ee4bfe118d6553c599754d46df29a
Lebogang Lottering <lebolottering2@outlook.com>
2022-04-11 13:20:25 +0200

    completed the tempConversion exercise, converted fahrenheit to celsius (and vice versa) and rounded it to one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function() {                     // conversion from fahrenheit to celsius
+  let temp = (arguments[0]-32)* 5/9;          // formulae for converting fahrenheit to celsius
+  let rounded = Math.round(temp * 10) / 10;   // rounds-off to one decimal place
+  return rounded;
 };
 
 const ctof = function() {
-
+  let temp = arguments[0]* 9/5 + 32;             // formulae for converting celsius to fahrenheit
+  let rounded = Math.round(temp * 10) / 10;      // rounds-off to one decimal place
+  return rounded;
 };
 
 // Do not edit below this line
