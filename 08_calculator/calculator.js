041c5641a4b549fe911e27e972cdd468b83decdf
Lebogang Lottering <lebolottering2@outlook.com>
2022-11-28 10:22:14 +0200

    completed the calcutor exercise and all functions work

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let unknown = numbers.reduce(function(a, b){
+    return a + b
+  }, 0)
+  return unknown
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let x = 1
+  for(i = 0; i < numbers.length; i++){
+    x = x * numbers[i]
+  }
+  return x;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	if(exponent === 0){
+    return 1;
+  }
+  else{
+    return base * power(base, exponent - 1);
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num >= 0 && num <= 1){
+    return 1;
+  }
+  else{
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
