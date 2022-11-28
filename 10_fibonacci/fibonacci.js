219a14b455045df751aa7d7b03253e2013ba0c9c
Lebogang Lottering <lebolottering2@outlook.com>
2022-11-28 10:53:29 +0200

    completed fibonacci exercise, functions returns specified members of fib sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+    if(fib <= 0){
+        return "OOPS"
+    }
+    else if(fib == 1){
+        return 1;
+    }
+    else if(fib == 2){
+        return 1;
+    }
+    else{
+        let arrayFib = [1,1];
 
+        for(let i = 1; i <= fib - 2; i++){
+            finalFib = arrayFib[i] + arrayFib[i-1];
+            arrayFib.push(finalFib); 
+        }
+        return finalFib;
+    }
 };
 
 // Do not edit below this line
