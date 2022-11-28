217a5cb07ac9810eeadd8e6b8f519884860149fe
Lebogang Lottering <lebolottering2@outlook.com>
2022-11-28 11:15:36 +0200

    completed exercise, function gets the title of the books

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(booksArray) {
+    return booksArray.map((item) => {
+        return item.title;
+    })
 };
 
 // Do not edit below this line
