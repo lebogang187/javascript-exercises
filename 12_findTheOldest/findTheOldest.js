b9e933684293b7012bea890d9a0c91148aed3e19
Lebogang Lottering <lebolottering2@outlook.com>
2022-11-28 11:32:58 +0200

    completed exercise, function returns oldest person from the array of object

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let sorted = people.sort((a, b) => {
 
+        //create year of death where it doesn't exist
+        if(!a.yearOfDeath){
+            a['yearOfDeath'] = new Date().getFullYear();
+        }
+        if(!b.yearOfDeath){
+            b['yearOfDeath'] = new Date().getFullYear();
+        }
+
+        //calculates the age
+        let aAge = a.yearOfDeath - a.yearOfBirth;
+        let bAge = b.yearOfDeath - b.yearOfBirth;
+        if(aAge > bAge){
+            return -1;                                             // returns from oldest to youngest
+        }
+        else{
+            return 1;
+        }
+    });
+    return sorted[0];
 };
 
 // Do not edit below this line
